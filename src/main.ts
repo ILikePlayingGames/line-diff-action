ac281be57f5117e77abecc06c97384dc715ff7d1
ILikePlayingGames <22475143+ILikePlayingGames@users.noreply.github.com>
2023-02-23 03:04:50 -0500

    Cache diff-so-fancy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import * as core from '@actions/core'
+import * as tc from '@actions/tool-cache'
 import * as tools from './command-line-tools'
 import {getDiffBetweenCommitAndHead} from './diff'
 
+async function downloadDiffSoFancy(): Promise<string> {
+  const diffSoFancyPath = await tc.downloadTool(
+    'https://github.com/so-fancy/diff-so-fancy/releases/download/v1.4.3/diff-so-fancy'
+  )
+  return await tc.cacheFile(
+    diffSoFancyPath,
+    'diff-so-fancy',
+    'diff-so-fancy',
+    '1.4.3'
+  )
+}
+
+async function loadDiffSoFancy(): Promise<void> {
+  let diffSoFancyDir = tc.find('diff-so-fancy', '1.4.3')
+
+  if (diffSoFancyDir === undefined) {
+    diffSoFancyDir = await downloadDiffSoFancy()
+  }
+  core.addPath(diffSoFancyDir)
+}
+
 async function run(): Promise<void> {
   try {
+    await loadDiffSoFancy()
     const commitHash: string = core.getInput('commit-hash')
     let diff = ''
 
