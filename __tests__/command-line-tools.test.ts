3a3f07c279e1feba92717523ca81089da0a387f7
ILikePlayingGames <22475143+ILikePlayingGames@users.noreply.github.com>
2023-02-23 01:36:44 -0500

    Fix test.yml for the third time

--- __tests__/command-line-tools.test.ts
+++ __tests__/command-line-tools.test.ts
@@ -1,39 +1,25 @@
 import * as commandLineTools from '../src/command-line-tools'
 import * as diffTool from '../src/diff'
-import {expect, test, describe} from '@jest/globals'
-import * as process from 'process'
+import {expect, test} from '@jest/globals'
 
-describe('tests that always run', () => {
-  test('diff-so-fancy setup', async () => {
-    await expect(
-      commandLineTools.setupDiffSoFancy()
-    ).resolves.not.toThrowError()
-  })
-
-  test("commit doesn't exist", async () => {
-    let hash = '7a118f3040c7cbe7373bc03783a3e65d5cd42cd5'
-    await expect(commandLineTools.doesCommitExist(hash)).resolves.toEqual(false)
-  })
+test('diff-so-fancy setup', async () => {
+  await expect(commandLineTools.setupDiffSoFancy()).resolves.not.toThrowError()
 })
 
-// These are impractical to run on GitHub runners
-describe("tests that don't run on GitHub runners", () => {
-  test('commit exists', async () => {
-    let hash = '7a118f3040c7cbe7373bc03783a3e65d5cd42cd4'
-    if (process.env.RUNNER_OS === undefined) {
-      await expect(commandLineTools.doesCommitExist(hash)).resolves.toEqual(
-        true
-      )
-    }
-  })
+test("commit doesn't exist", async () => {
+  let hash = '7a118f3040c7cbe7373bc03783a3e65d5cd42cd5'
+  await expect(commandLineTools.doesCommitExist(hash)).resolves.toEqual(false)
+})
 
-  test('diff between commits', async () => {
-    let hashOne = '7a118f3040c7cbe7373bc03783a3e65d5cd42cd4'
-    let hashTwo = '567215cb1083813b143f15526175382ee049d8e0'
-    if (process.env.RUNNER_OS === undefined) {
-      await expect(
-        diffTool.getDiffBetweenCommits(hashOne, hashTwo)
-      ).resolves.not.toBeUndefined()
-    }
-  })
+test('commit exists', async () => {
+  let hash = '@'
+  await expect(commandLineTools.doesCommitExist(hash)).resolves.toEqual(true)
+})
+
+test('diff between commits', async () => {
+  let hashOne = '@~'
+  let hashTwo = '@'
+  await expect(
+    diffTool.getDiffBetweenCommits(hashOne, hashTwo)
+  ).resolves.not.toBeUndefined()
 })
