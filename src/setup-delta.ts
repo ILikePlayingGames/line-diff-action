497ee8c55a2e4534640b2beb5b713f508623748b
ILikePlayingGames <22475143+ILikePlayingGames@users.noreply.github.com>
2023-02-27 00:20:35 -0500

    Try to fix extracted path again

--- src/setup-delta.ts
+++ src/setup-delta.ts
@@ -1,6 +1,6 @@
 import * as tc from '@actions/tool-cache'
 import * as core from '@actions/core'
-import {execCommands, makeFileExecutable} from './command-line-tools'
+import {execCommands} from './command-line-tools'
 
 const deltaVersion = '0.15.1'
 
@@ -8,42 +8,31 @@ async function downloadDelta(): Promise<string> {
   let deltaPath
   let deltaExtractedFolder
   let deltaPlatform
-  let executableName
 
   if (process.platform === 'win32') {
     deltaPlatform = 'x86_64-pc-windows-msvc'
     deltaPath = await tc.downloadTool(
       `https://github.com/dandavison/delta/releases/download/${deltaVersion}/delta-${deltaVersion}-${deltaPlatform}.zip`
     )
-    deltaExtractedFolder = `${await tc.extractZip(
-      deltaPath
-    )}/delta-${deltaVersion}-${deltaPlatform}`
-    executableName = 'delta.exe'
+    deltaExtractedFolder = await tc.extractZip(deltaPath)
   } else if (process.platform === 'darwin') {
     deltaPlatform = 'x86_64-apple-darwin'
     deltaPath = await tc.downloadTool(
       `https://github.com/dandavison/delta/releases/download/${deltaVersion}/delta-${deltaVersion}-${deltaPlatform}.tar.gz`
     )
-    deltaExtractedFolder = `${await tc.extractTar(
-      deltaPath
-    )}/delta-${deltaVersion}-${deltaPlatform}`
-    executableName = 'delta'
+    deltaExtractedFolder = await tc.extractTar(deltaPath)
   } else {
     deltaPlatform = 'x86_64-unknown-linux-gnu'
     deltaPath = await tc.downloadTool(
       `https://github.com/dandavison/delta/releases/download/${deltaVersion}/delta-${deltaVersion}-${deltaPlatform}.tar.gz`
     )
-    deltaExtractedFolder = `${await tc.extractTar(
-      deltaPath
-    )}/delta-${deltaVersion}-${deltaPlatform}`
-    executableName = 'delta'
+    deltaExtractedFolder = await tc.extractTar(deltaPath)
   }
 
   core.info(`Downloaded Delta ${deltaVersion} for ${process.platform}`)
 
-  const cachedPath = await tc.cacheFile(
-    `${deltaExtractedFolder}/${executableName}`,
-    executableName,
+  const cachedPath = await tc.cacheDir(
+    deltaExtractedFolder,
     'delta',
     deltaVersion
   )
@@ -53,7 +42,7 @@ async function downloadDelta(): Promise<string> {
   return cachedPath
 }
 
-async function setupDelta(deltaDir: string): Promise<void> {
+async function setupDelta(): Promise<void> {
   await execCommands([
     'git config --local core.pager "delta"',
     'git config --local interactive.diffFilter "delta --color-only"',
@@ -61,10 +50,6 @@ async function setupDelta(deltaDir: string): Promise<void> {
     'git config --local merge.conflictStyle "diff3"',
     'git config --local diff.colorMoved "default"'
   ])
-
-  if (process.platform !== 'win32') {
-    await makeFileExecutable(`${deltaDir}/delta`)
-  }
 }
 
 export async function loadDelta(): Promise<void> {
@@ -78,5 +63,5 @@ export async function loadDelta(): Promise<void> {
   }
 
   core.addPath(deltaDir)
-  await setupDelta(deltaDir)
+  await setupDelta()
 }
